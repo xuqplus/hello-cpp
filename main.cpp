f73e002c2f020f294072aade7ab690e060c8b88f
xuqplus <xuqunqun@live.cn>
2021-08-28 12:44:07 +0800

    TBD - CURL library debug failed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <curl/curl.h>
+
+using namespace std;
+
+void test_curl()
+{
+    CURL *curl;
+    CURLcode res;
+    // curl_global_init(CURL_GLOBAL_DEFAULT);
+    // curl = curl_easy_init();
+    // if (curl)
+    // {
+    //     curl_easy_setopt(curl, CURLOPT_URL, "https://example.com/");
+    //     res = curl_easy_perform(curl);
+    //     /* Check for errors */
+    //     if (res != CURLE_OK)
+    //         fprintf(stderr, "curl_easy_perform() failed: %s\n",
+    //                 curl_easy_strerror(res));
+    //     /* always cleanup */
+    //     curl_easy_cleanup(curl);
+    // }
+    // curl_global_cleanup();
+}
 
 int main(int, char **)
 {
     std::cout << "hello cpp .." << std::endl;
+
+    test_curl();
     return 0;
 }
