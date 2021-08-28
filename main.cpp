f4360f705819107c219827dc815dc0e4ba5e1fc2
xuqplus <xuqunqun@live.cn>
2021-08-28 21:06:47 +0800

    Fix debug issue by move vcpkg including to the top

--- main.cpp
+++ main.cpp
@@ -33,20 +33,20 @@ void test_curl()
 {
     CURL *curl;
     CURLcode res;
-    // curl_global_init(CURL_GLOBAL_DEFAULT);
-    // curl = curl_easy_init();
-    // if (curl)
-    // {
-    //     curl_easy_setopt(curl, CURLOPT_URL, "https://example.com/");
-    //     res = curl_easy_perform(curl);
-    //     /* Check for errors */
-    //     if (res != CURLE_OK)
-    //         fprintf(stderr, "curl_easy_perform() failed: %s\n",
-    //                 curl_easy_strerror(res));
-    //     /* always cleanup */
-    //     curl_easy_cleanup(curl);
-    // }
-    // curl_global_cleanup();
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://example.com/");
+        res = curl_easy_perform(curl);
+        /* Check for errors */
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
 }
 
 int main(int, char **)
